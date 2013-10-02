b91f199741d09249242bab06e064a405561e1214
John Bender <john.m.bender@gmail.com>
2013-10-02 15:45:14 -0700

    started on 1a

--- homework/1/hw1.ml
+++ homework/1/hw1.ml
@@ -1,5 +1,6 @@
 
 exception ImplementMe
+exception StepError
 
 type t = True | False | If of t * t * t | Int of int | Plus of t * t | GT of t * t
 
@@ -11,12 +12,32 @@ let isval t =
 
 (* Problem 1a.  *)
 
-exception NormalForm 
+exception NormalForm
 
-let rec step t = raise ImplementMe
+let rec step t =
+  match t with
+      If(guard, thn, els) ->
+        (match guard with
+            True -> thn
+          | False -> els
+          | _ -> If(step guard, thn, els))
+    | Plus(t1, t2) ->
+      (match (t1, t2) with
+          (Int n1, Int n2) -> Int (n1 + n2)
+        | (Int n, t) -> Plus(Int n, step t)
+        | (t1, t2) -> Plus(step t1, t2))
+    | GT(t1, t2) ->
+      (match (t1, t2) with
+          (Int n1, Int n2) -> (if n1 > n2 then True else False)
+        | (Int n, t) -> GT(Int n, step t)
+        | (t1, t2) -> GT(step t1, t2))
+    | _ -> raise StepError
 
+(* step(Plus(Int 1, Int 2));; *)
+(* step(Plus(Plus(Int 1, Int 3), Int 2));; *)
+(* step(GT(Int 1, Int 3));; *)
+(* step(If(GT(Int 1, Int 3), True, False));; *)
 
 (* Problem 1b. *)
-  
-let rec eval t = raise ImplementMe
 
+let rec eval t = raise ImplementMe
