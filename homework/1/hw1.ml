6fa11b23a4b4fc74c096399ad8b023de16c26418
John Bender <john.m.bender@gmail.com>
2013-10-02 22:58:15 -0700

    whitespace to clarify

--- homework/1/hw1.ml
+++ homework/1/hw1.ml
@@ -1,5 +1,4 @@
-
-exception ImplementMe
+exception NormalForm
 
 type t = True | False | If of t * t * t | Int of int | Plus of t * t | GT of t * t
 
@@ -11,8 +10,6 @@ let isval t =
 
 (* Problem 1a.  *)
 
-exception NormalForm
-
 let rec step t =
   match t with
       If(guard, thn, els) ->
@@ -20,16 +17,19 @@ let rec step t =
             True -> thn
           | False -> els
           | _ -> If(step guard, thn, els))
+
     | Plus(t1, t2) ->
       (match (t1, t2) with
           (Int n1, Int n2) -> Int (n1 + n2)
         | (Int n, t) -> Plus(Int n, step t)
         | (t1, t2) -> Plus(step t1, t2))
+
     | GT(t1, t2) ->
       (match (t1, t2) with
           (Int n1, Int n2) -> (if n1 > n2 then True else False)
         | (Int n, t) -> GT(Int n, step t)
         | (t1, t2) -> GT(step t1, t2))
+
     | _ -> raise NormalForm
 
 (* step(Plus(Int 1, Int 2));; *)
