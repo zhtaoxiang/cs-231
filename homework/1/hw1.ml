666aabeca997f0177f29554e6bb8d1699f8b08dc
John Bender <john.m.bender@gmail.com>
2013-10-03 21:45:52 -0700

    fully evaluate everything before throwing NormalForm (the exception says it all

--- homework/1/hw1.ml
+++ homework/1/hw1.ml
@@ -4,14 +4,15 @@ type t = True | False | If of t * t * t | Int of int | Plus of t * t | GT of t *
 
 let isval t =
   match t with
-      True|False -> true
+      True
+    | False -> true
     | Int _ -> true
     | _ -> false
 
 (* Problem 1a.  *)
 
 let rec step t =
-  match t with
+  (match t with
       If(guard, thn, els) ->
         (match guard with
             True -> thn
@@ -21,16 +22,27 @@ let rec step t =
     | Plus(t1, t2) ->
       (match (t1, t2) with
           (Int n1, Int n2) -> Int (n1 + n2)
-        | (Int n, t) -> Plus(Int n, step t)
-        | (t1, t2) -> Plus(step t1, t2))
+        | (t1, t2) ->
+          if not(isval(t1))
+          then Plus(step t1, t2)
+          else
+            (if not(isval(t2))
+             then Plus(t1, step t2)
+             else raise NormalForm))
 
     | GT(t1, t2) ->
       (match (t1, t2) with
           (Int n1, Int n2) -> (if n1 > n2 then True else False)
-        | (Int n, t) -> GT(Int n, step t)
-        | (t1, t2) -> GT(step t1, t2))
+        | (t1, t2) ->
+          if not(isval(t1))
+          then GT(step t1, t2)
+          else
+            (if not(isval(t2))
+             then GT(t1, step t2)
+             else raise NormalForm))
 
-    | _ -> raise NormalForm
+    | _ -> raise NormalForm)
+
 
 (* step(Plus(Int 1, Int 2));; *)
 (* step(Plus(Plus(Int 1, Int 3), Int 2));; *)
